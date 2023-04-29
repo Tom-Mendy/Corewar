1059f62fc2e2efc84dc427d0e932d4ddfe6fd80e
Tom Mendy <tom.mendy@epitech.eu>
2023-04-29 15:13:38 +0200

    chore: add *.s to .gitignore to ignore assembly files test(my_char_map_len): refactor test cases to improve readability and consistency

--- lib/my_char_map/tests/test_my_char_map_len.c
+++ lib/my_char_map/tests/test_my_char_map_len.c
@@ -8,21 +8,18 @@
 #include <criterion/criterion.h>
 #include "my_char_map.h"
 
-Test(my_char_map_len, null_map)
-{
+Test(my_char_map_len, null_map) {
     cr_assert_eq(my_char_map_len(NULL), -1);
 }
 
-Test(my_char_map_len, empty_map)
-{
+Test(my_char_map_len, empty_map) {
     char **map = malloc(sizeof(char*));
     map[0] = NULL;
     cr_assert_eq(my_char_map_len(map), 0);
     free(map);
 }
 
-Test(my_char_map_len, one_row_map)
-{
+Test(my_char_map_len, one_row_map) {
     char **map = malloc(sizeof(char*) * 2);
     map[0] = "This is one row map.";
     map[1] = NULL;
@@ -30,8 +27,7 @@ Test(my_char_map_len, one_row_map)
     free(map);
 }
 
-Test(my_char_map_len, three_rows_map)
-{
+Test(my_char_map_len, three_rows_map) {
     char **map = malloc(sizeof(char*) * 4);
     map[0] = "This is three rows map.";
     map[1] = "And this is the second row.";
@@ -40,4 +36,3 @@ Test(my_char_map_len, three_rows_map)
     cr_assert_eq(my_char_map_len(map), 3);
     free(map);
 }
-
