35670f815ed74359c0e833483c3fc5e2d1ef49e3
Tom Mendy <tom.mendy@epitech.eu>
2023-05-09 10:56:22 +0200

    [FIX] asm header using macro

--- asm/src/find_name_comment.c
+++ asm/src/find_name_comment.c
@@ -12,11 +12,11 @@
 
 static int find_name(char *actual_line, int const j, char **name)
 {
-    if (my_str_n_cmp(&(actual_line[j]), ".name", 5) == 0){
+    if (my_str_n_cmp(&(actual_line[j]), NAME_CMD_STRING, 5) == 0){
         if (*name != NULL)
             return KO;
         char **azerty = spliter(actual_line, "\"");
-        int nb_hashtag = my_count_nb_char_in_str(azerty[0], '#');
+        int nb_hashtag = my_count_nb_char_in_str(azerty[0], COMMENT_CHAR);
         if (nb_hashtag != 0)
             return OK;
         *name = generate_malloc_str_from_str(azerty[1]);
@@ -28,11 +28,11 @@ static int find_name(char *actual_line, int const j, char **name)
 
 static int find_comment(char *actual_line, int const j, char **comment)
 {
-    if (my_str_n_cmp(&(actual_line[j]), ".comment", 8) == 0){
+    if (my_str_n_cmp(&(actual_line[j]), COMMENT_CMD_STRING, 8) == 0){
         if (*comment != NULL)
             return KO;
         char **azerty = spliter(actual_line, "\"");
-        int nb_hashtag = my_count_nb_char_in_str(azerty[0], '#');
+        int nb_hashtag = my_count_nb_char_in_str(azerty[0], COMMENT_CHAR);
         if (nb_hashtag != 0)
             return OK;
         *comment = generate_malloc_str_from_str(azerty[1]);
@@ -45,7 +45,7 @@ static int find_comment(char *actual_line, int const j, char **comment)
 int find_name_comment(char *actual_line, char **name, char **comment)
 {
     for (int j = 0; actual_line[j] != '\0'; j += 1){
-        if (actual_line[j] == '#')
+        if (actual_line[j] == COMMENT_CHAR)
             return 0;
         if (find_name(actual_line, j, name) == OK)
             return OK;
