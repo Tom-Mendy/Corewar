568af569912329e3e9563727b2d1fd2c9e3ceced
Tom Mendy <tom.mendy@epitech.eu>
2023-05-09 11:18:46 +0200

    [FIX] rigor

--- asm/src/asm.c
+++ asm/src/asm.c
@@ -13,6 +13,8 @@
 int put_name_and_comment_in_struct(header_t *header_file, char *name,
 char *comment)
 {
+    if (header_file == NULL || name == NULL || comment == NULL)
+        return KO;
     for (int i = 0; i < PROG_NAME_LENGTH; i += 1){
         header_file->prog_name[i] = 0;
     }
@@ -33,33 +35,48 @@ int init_header_file(header_t *header_file, char *filename)
     char *name = NULL;
     char *comment = NULL;
     char **file_in_array = my_load_file_in_array(filename);
+
     if (file_in_array == NULL)
         return KO;
     for (int i = 0; file_in_array[i] != NULL; i++) {
-        find_name_comment(file_in_array[i], &name, &comment);
+        if (find_name_comment(file_in_array[i], &name, &comment) == KO)
+            return KO;
     }
     if (header_file == NULL || name == NULL || comment == NULL)
         return KO;
     header_file->magic = big_endian_number(COREWAR_EXEC_MAGIC);
     header_file->prog_size = big_endian_number(22);
-    put_name_and_comment_in_struct(header_file, name, comment);
+    if (put_name_and_comment_in_struct(header_file, name, comment) == KO)
+        return KO;
+    return OK;
+}
+
+int write_to_file(char *output_filename, header_t *header_file)
+{
+    int output_fd = open(output_filename, O_CREAT | O_WRONLY, S_IRWXU);
+
+    if (output_fd == -1)
+        return KO;
+    write(output_fd, header_file, sizeof(header_t));
+    close(output_fd);
     return OK;
 }
 
 int asm_function(char *filename)
 {
     char *output_filename = generate_output_filename(filename);
+    header_t *header_file = NULL;
+
     if (output_filename == NULL)
         return 84;
-    header_t *header_file = malloc(sizeof(header_t));
+    header_file = malloc(sizeof(header_t));
     if (header_file == NULL)
         return 84;
     if (init_header_file(header_file, filename) == KO)
         return KO;
-    int output_fd = open(output_filename, O_CREAT | O_WRONLY, S_IRWXU);
-    if (output_fd == -1)
+    if (write_to_file(output_filename, header_file) == KO)
         return KO;
-    write(output_fd, header_file, sizeof(header_t));
-    close(output_fd);
+    free(output_filename);
+    free(header_file);
     return OK;
 }
--- asm/src/find_name_comment.c
+++ asm/src/find_name_comment.c
@@ -12,6 +12,8 @@
 
 static int find_name(char *actual_line, int const j, char **name)
 {
+    if (actual_line == NULL || name == NULL)
+        return KO;
     if (my_str_n_cmp(&(actual_line[j]), NAME_CMD_STRING, 5) == 0){
         if (*name != NULL)
             return KO;
@@ -26,8 +28,13 @@ static int find_name(char *actual_line, int const j, char **name)
     return 1;
 }
 
-static int find_comment(char *actual_line, int const j, char **comment)
+static int find_comment(char *actual_line, int const j, char **comment,
+char **name)
 {
+    if (actual_line == NULL || comment == NULL || name == NULL)
+        return KO;
+    if (*name == NULL)
+        return 1;
     if (my_str_n_cmp(&(actual_line[j]), COMMENT_CMD_STRING, 8) == 0){
         if (*comment != NULL)
             return KO;
@@ -44,6 +51,8 @@ static int find_comment(char *actual_line, int const j, char **comment)
 
 int find_name_comment(char *actual_line, char **name, char **comment)
 {
+    if (actual_line == NULL || comment == NULL || name == NULL)
+        return KO;
     for (int j = 0; actual_line[j] != '\0'; j += 1){
         if (actual_line[j] == COMMENT_CHAR)
             return 0;
