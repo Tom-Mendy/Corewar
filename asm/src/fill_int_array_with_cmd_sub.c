88eaf4b4a2e6a4e97ba5bba854bfa0c687f15d8d
Tom Mendy <tom.mendy@epitech.eu>
2023-05-24 17:16:40 +0200

    [ADD] register handling

--- asm/src/fill_int_array_with_cmd_sub.c
+++ asm/src/fill_int_array_with_cmd_sub.c
@@ -9,10 +9,9 @@
 #include "asm.h"
 #include "op.h"
 
-static int add_len_by_type_of_param(char *str, int *len_instruction,
-asm_t *asm_n)
+static int add_len_by_type_of_param(char *str, int *len_instruction)
 {
-    if (!str || !len_instruction || !asm_n)
+    if (!str || !len_instruction)
         return KO;
     if (is_register(str) == true) {
         *len_instruction += T_REG;
@@ -26,7 +25,7 @@ asm_t *asm_n)
     return OK;
 }
 
-int add_info_in_array(asm_t *asm_n, int j, int len_instruction)
+int add_default_value_in_array(asm_t *asm_n, int j, int len_instruction)
 {
     asm_n->tab_int[asm_n->index_int_tab] = malloc(sizeof(int) *
     (len_instruction + 1));
@@ -40,6 +39,27 @@ int add_info_in_array(asm_t *asm_n, int j, int len_instruction)
     return OK;
 }
 
+int add_value_in_array(char **split_line, asm_t *asm_n, int h)
+{
+    int len_instruction = 2;
+    int this_instruction = 0;
+    for (int k = 1 + h; split_line[k] != NULL; k += 1) {
+        this_instruction = len_instruction;
+        if (add_len_by_type_of_param(split_line[k], &len_instruction) == KO)
+            return KO;
+        this_instruction = len_instruction - this_instruction;
+        switch (this_instruction) {
+        case 1:
+            asm_n->tab_int[asm_n->index_int_tab][len_instruction - 1] =
+            my_get_nbr(&(split_line[k][1]));
+            break;
+        default:
+            break;
+        }
+    }
+    return OK;
+}
+
 int fill_int_array_with_cmd_sub(char **split_line, int j, asm_t *asm_n, int h)
 {
     int len_instruction = 2;
@@ -49,9 +69,12 @@ int fill_int_array_with_cmd_sub(char **split_line, int j, asm_t *asm_n, int h)
     if ((my_char_map_len(split_line) - h) != op_tab[j].nbr_args + 1)
         return KO;
     for (int k = 1; split_line[k] != NULL; k += 1) {
-        add_len_by_type_of_param(split_line[k], &len_instruction, asm_n);
+        if (add_len_by_type_of_param(split_line[k], &len_instruction) == KO)
+            return KO;
     }
-    if (add_info_in_array(asm_n, j, len_instruction) == KO)
+    if (add_default_value_in_array(asm_n, j, len_instruction) == KO)
+        return KO;
+    if (add_value_in_array(split_line, asm_n, h) == KO)
         return KO;
     asm_n->index_int_tab += 1;
     return OK;
