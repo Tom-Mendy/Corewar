998bbf9dc072eeda1edca6b2ebeecbce14921a42
Tom Mendy <tom.mendy@epitech.eu>
2023-05-24 15:50:27 +0200

    [UPD] check_instruction_validity.c

--- asm/src/check_instruction_validity.c
+++ asm/src/check_instruction_validity.c
@@ -16,7 +16,7 @@ static int do_if(char **line, int i)
     int j = 0;
     for (j = 0; j < 17; j++) {
         if (my_str_n_cmp(line[i], op_tab[j].mnemonique,
-        my_str_len(op_tab[i].mnemonique)) == 0) {
+            my_str_len(op_tab[i].mnemonique)) == 0) {
             write(2, "Error: Multiple mnemonics found on same line.\n", 47);
             return -1;
         }
@@ -27,7 +27,7 @@ static int do_if(char **line, int i)
 static int check_mn_per_line(char **line)
 {
     int i = 1;
-    char label_str[2] = {LABEL_CHAR, 0};
+    char label_str[2] = { LABEL_CHAR, 0 };
 
     if (!line)
         return -1;
@@ -37,7 +37,7 @@ static int check_mn_per_line(char **line)
         if (do_if(line, i) == -1)
             return -1;
     }
-    return 0;
+    return OK;
 }
 
 int get_len_op_tab(void)
@@ -47,25 +47,32 @@ int get_len_op_tab(void)
     return i;
 }
 
+int error(char **line, int i, int len_op_tab)
+{
+    if ((i + 1) == len_op_tab) {
+        write(2, "Error: Line without mnemonic, or LABEL error.\n", 47);
+        my_free_char_map(line);
+        return -1;
+    }
+    return OK;
+}
+
 static int check_if_mn(char *str)
 {
     int j = 0;
     int return_value = 0;
     char **line = spliter(str, " ");
-    char label_str[2] = {LABEL_CHAR, 0};
+    char label_str[2] = { LABEL_CHAR, 0 };
     int len_op_tab = get_len_op_tab();
 
     if (my_str_cmp(my_str_str(line[0], label_str), label_str) == 0)
         j = 1;
     for (int i = 0; i < len_op_tab; i++) {
         if (my_str_n_cmp(line[j], op_tab[i].mnemonique,
-        my_str_len(op_tab[i].mnemonique)) == 0)
+            my_str_len(op_tab[i].mnemonique)) == 0)
             break;
-        if ((i + 1) == len_op_tab) {
-            write(2, "Error: Line without mnemonic, or LABEL error.\n", 47);
-            my_free_char_map(line);
+        if (error(line, i, len_op_tab) == -1)
             return -1;
-        }
     }
     return_value = check_mn_per_line(line);
     my_free_char_map(line);
@@ -82,5 +89,5 @@ int check_info(char **arr)
         if (check_if_mn(arr[i]) == -1)
             return -1;
     }
-    return 0;
+    return OK;
 }
