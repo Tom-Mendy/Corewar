1dc2ead4c76969c316fbab0aa962d595fc83671b
Tom Mendy <tom.mendy@epitech.eu>
2023-05-25 16:57:26 +0200

    [FIX] name & comment handling

--- asm/src/asm_function/find_name_comment.c
+++ asm/src/asm_function/find_name_comment.c
@@ -10,60 +10,52 @@
 #include "asm.h"
 #include "op.h"
 
-static int find_name(char *actual_line, int const j, char **name)
-{
-    if (actual_line == NULL || name == NULL || j < 0)
-        return KO;
-    if (my_str_n_cmp(&(actual_line[j]), NAME_CMD_STRING,
-    my_str_len(NAME_CMD_STRING)) == 0){
-        if (*name != NULL)
-            return KO;
-        char **cote = spliter(actual_line, "\"");
-        int nb_hashtag = my_count_nb_char_in_str(cote[0], COMMENT_CHAR);
-        if (nb_hashtag != 0)
-            return OK;
-        *name = generate_malloc_str_from_str(cote[1]);
-        my_free_char_map(cote);
-        return OK;
-    }
-    return 1;
-}
-
-static int find_comment(char *actual_line, int const j, char **comment)
-{
-    if (actual_line == NULL || comment == NULL || j < 0)
-        return KO;
-    if (my_str_n_cmp(&(actual_line[j]), COMMENT_CMD_STRING,
-    my_str_len(COMMENT_CMD_STRING)) == 0){
-        if (*comment != NULL)
-            return KO;
-        char **cote = spliter(actual_line, "\"");
-        int nb_hashtag = my_count_nb_char_in_str(cote[0], COMMENT_CHAR);
-        if (nb_hashtag != 0)
-            return OK;
-        *comment = generate_malloc_str_from_str(cote[1]);
-        my_free_char_map(cote);
-        return OK;
-    }
-    return 1;
-}
-
 int find_name_in_line(char *actual_line, char **name)
 {
-    for (int j = 0; actual_line[j] != '\0'; j += 1){
-        if (find_name(actual_line, j, name) == OK)
-            return OK;
+    char **split_line = spliter(actual_line, " \t");
+    if (split_line == NULL)
+        return KO;
+    if (my_str_cmp(split_line[0], NAME_CMD_STRING) == 0) {
+        if (my_count_nb_char_in_str(actual_line, '\"') != 2)
+            return KO;
+        char **cote = spliter(actual_line, "\"");
+        if (cote == NULL)
+            return KO;
+        if (my_char_map_len(cote) != 2)
+            return KO;
+        *name = generate_malloc_str_from_str(cote[1]);
+        my_free_char_map(cote);
+        if (my_str_len(*name) > PROG_NAME_LENGTH){
+            free(*name);
+            return KO;
+        }
+        return OK;
     }
-    return 1;
+    return KO;
 }
 
 int find_comment_in_line(char *actual_line, char **comment)
 {
-    for (int j = 0; actual_line[j] != '\0'; j += 1){
-        if (find_comment(actual_line, j, comment) == OK)
-            return OK;
+    char **split_line = spliter(actual_line, " \t");
+    if (split_line == NULL)
+        return KO;
+    if (my_str_cmp(split_line[0], COMMENT_CMD_STRING) == 0){
+        if (my_count_nb_char_in_str(actual_line, '\"') != 2)
+            return KO;
+        char **cote = spliter(actual_line, "\"");
+        if (cote == NULL)
+            return KO;
+        if (my_char_map_len(cote) != 2)
+            return KO;
+        *comment = generate_malloc_str_from_str(cote[1]);
+        my_free_char_map(cote);
+        if (my_str_len(*comment) > COMMENT_LENGTH){
+            free(*comment);
+            return KO;
+        }
+        return OK;
     }
-    return 1;
+    return KO;
 }
 
 int find_name_comment(char **file_in_array, char **name, char **comment)
