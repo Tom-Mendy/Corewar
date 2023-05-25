032468bd9acf6e3fe8354309e36b7b45c12bbadb
Tom Mendy <tom.mendy@epitech.eu>
2023-05-25 14:49:55 +0200

    [ADD] coding byte struct

--- corewar/include/my_vm.h
+++ corewar/include/my_vm.h
@@ -20,6 +20,13 @@
         champion_list_t *champion_list;
         champion_t **tab_champion;
     } my_vm_t;
+    typedef struct decimal_to_code_byte_s{
+        char option_1[3];
+        char option_2[3];
+        char option_3[3];
+        char option_4[3];
+    }decimal_to_code_byte_t;
     void free_vm(my_vm_t *my_vm);
+    int decimal_to_code_byte(int nb);
 
 #endif /* !MY_VM_H_ */
