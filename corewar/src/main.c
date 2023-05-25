78a701ba3dab1f632a1243856601560d9dbd172f
Matheo PIQUES <matheo.piques@epitech.eu>
2023-05-25 11:12:19 +0200

    [FIX] number of argc

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -12,7 +12,7 @@ int main(int const argc, char const *argv[])
 {
     if (my_str_cmp(argv[1], "-h") == 0 && argc == 2)
         return display_help();
-    if (argc < 5) {
+    if (argc < 3) {
         if (write(2, "Not enough argument.\n", 22) == -1)
             return KO;
         return KO;
